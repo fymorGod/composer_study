5c8b3881d878d5f89da778328b8bd8efcbfa14bb
fymorgod <mufinagamer@gmail.com>
2021-08-15 18:57:17 -0300

    Agr PSr-4 att

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -1,18 +1,17 @@
 <?php 
     require 'vendor/autoload.php';
+
+    use ALura\Buscador\Buscador;
     use GuzzleHttp\Client;
     use Symfony\Component\DomCrawler\Crawler as DomCrawlerCrawler;
-    $client = new Client(['verify' => false]);
-    $uri = 'https://www.alura.com.br/cursos-online-programacao/php';
+    $client = new Client(['verify' => false, 'base_uri'=> 'https://www.alura.com.br/']);
  
-    $response = $client->request('GET', $uri);
-    $html = $response->getBody();
     $crawler = new DomCrawlerCrawler();
 
     $crawler->addHtmlContent($html);
-
-
-    $cursos = $crawler->filter('span.card-curso__nome');
+    
+    $buscador = new Buscador($client, $crawler);
+    $cursos = $buscador->buscar('cursos-online-programacao/php');
 
     foreach($cursos as $curso){
         echo $curso->textContent . PHP_EOL;
