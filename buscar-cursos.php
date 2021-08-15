42cfb74993513b54a21d155e5a51aa45affe6d14
fymorgod <mufinagamer@gmail.com>
2021-08-15 18:57:51 -0300

    correct line

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -7,8 +7,6 @@
     $client = new Client(['verify' => false, 'base_uri'=> 'https://www.alura.com.br/']);
  
     $crawler = new DomCrawlerCrawler();
-
-    $crawler->addHtmlContent($html);
     
     $buscador = new Buscador($client, $crawler);
     $cursos = $buscador->buscar('cursos-online-programacao/php');
